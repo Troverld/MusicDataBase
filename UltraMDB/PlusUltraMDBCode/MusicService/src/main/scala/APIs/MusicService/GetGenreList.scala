2c2559d60ac25ff9b098a19a3ed1c946dada3756
asmend <3099007404@qq.com>
2025-07-01 14:41:19 +0800

    GetGenreList API

---
+++ UltraMDB/PlusUltraMDBCode/MusicService/src/main/scala/APIs/MusicService/GetGenreList.scala
@@ -0,0 +1,56 @@
+package APIs.MusicService
+
+import Common.API.API
+import Global.ServiceCenter.MusicServiceCode
+import io.circe.{Decoder, Encoder, Json}
+import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
+import io.circe.syntax.*
+import io.circe.parser.*
+import Common.Serialize.CustomColumnTypes.{decodeDateTime, encodeDateTime}
+import com.fasterxml.jackson.core.`type`.TypeReference
+import Common.Serialize.JacksonSerializeUtils
+import Objects.MusicService.Genre
+
+import scala.util.Try
+import org.joda.time.DateTime
+
+import java.util.UUID
+
+
+
+case class GetGenreList(
+  userID: String,
+  userToken: String,
+) extends API[(List[Genre], String)](MusicServiceCode)
+
+
+
+case object GetGenreList{
+    
+  import Common.Serialize.CustomColumnTypes.{decodeDateTime,encodeDateTime}
+
+  // Circe 默认的 Encoder 和 Decoder
+  private val circeEncoder: Encoder[DeleteGenre] = deriveEncoder
+  private val circeDecoder: Decoder[DeleteGenre] = deriveDecoder
+
+  // Jackson 对应的 Encoder 和 Decoder
+  private val jacksonEncoder: Encoder[DeleteGenre] = Encoder.instance { currentObj =>
+    Json.fromString(JacksonSerializeUtils.serialize(currentObj))
+  }
+
+  private val jacksonDecoder: Decoder[DeleteGenre] = Decoder.instance { cursor =>
+    try { Right(JacksonSerializeUtils.deserialize(cursor.value.noSpaces, new TypeReference[DeleteGenre]() {})) } 
+    catch { case e: Throwable => Left(io.circe.DecodingFailure(e.getMessage, cursor.history)) }
+  }
+  
+  // Circe + Jackson 兜底的 Encoder
+  given deleteGenreEncoder: Encoder[DeleteGenre] = Encoder.instance { config =>
+    Try(circeEncoder(config)).getOrElse(jacksonEncoder(config))
+  }
+
+  // Circe + Jackson 兜底的 Decoder
+  given deleteGenreDecoder: Decoder[DeleteGenre] = Decoder.instance { cursor =>
+    circeDecoder.tryDecode(cursor).orElse(jacksonDecoder.tryDecode(cursor))
+  }
+
+}
