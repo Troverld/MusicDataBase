2bf558945c24431593cea0d403246e1740ad1c98
larryzhong <larry.zhong@outlook.com>
2025-07-10 21:13:17 +0800

    Lorem ipsum dolor sit amet.

--- UltraMDB/PlusUltraMDBCode/music-frontend/src/pages/Dashboard.tsx
+++ UltraMDB/PlusUltraMDBCode/music-frontend/src/pages/Dashboard.tsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { getUser } from '../../utils/storage';
-import { usePermissions } from '../../hooks/usePermissions';
-import { statisticsService } from '../../services/statistics.service';
+import { getUser } from '../utils/storage';
+import { usePermissions } from '../hooks/usePermissions';
+import { statisticsService } from '../services/statistics.service';
 
 const Dashboard: React.FC = () => {
   const user = getUser();
